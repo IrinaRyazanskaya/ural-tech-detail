1b8a5bc382118b6253e14bee1f4368d57de7a037
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-01-06 17:39:18 +0300

    Сверстала страницу "О компании"

---
+++ src/application/application.jsx
@@ -0,0 +1,14 @@
+import { AboutPage } from '../pages/about';
+
+function Application() {
+    return (
+        <div>
+            <div>Навигация</div>
+            <div>Шапка</div>
+            <AboutPage />
+            <div>Подвал</div>
+        </div>
+    )
+}
+
+export { Application };
