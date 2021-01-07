3f6c30d070d08098235293ed9838efb556259313
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-01-08 00:31:20 +0300

    Сверстала навигацию

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,65 @@
+import './navigation.css';
+
+function Navigation(){
+    return (
+        <nav className="navigation">
+            <ul className="navigation__items">
+                <li className="navigation__item">
+                    <a className="navigation__item-link" href="">
+                        Главная
+                    </a>
+                </li>
+                <li className="navigation__item">
+                    <details className="navigation__sub-menu-container" href="">
+                        <summary className="navigation__trigger">О компании</summary>
+                            <nav className="navigation__sub-menu">
+                                <ul className="navigation__sub-menu-items">
+                                    <li className="navigation__sub-menu-item">
+                                        <a 
+                                            className="navigation__sub-menu-link"
+                                            href=""
+                                        >
+                                                Доставка и оплата
+                                        </a>
+                                    </li>
+                                    <li className="navigation__sub-menu-item">
+                                        <a 
+                                            className="navigation__sub-menu-link"
+                                            href=""
+                                        >
+                                                Гарантии и возврат
+                                        </a>
+                                    </li>
+                                    <li className="navigation__sub-menu-item">
+                                        <a 
+                                            className="navigation__sub-menu-link"
+                                            href=""
+                                        >
+                                            Реквизиты
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                    </details>
+                </li>
+                <li className="navigation__item">
+                    <a className="navigation__item-link" href="">
+                        Покупателю
+                    </a>
+                </li>
+                <li className="navigation__item">
+                    <a className="navigation__item-link" href="">
+                        Сотрудничество
+                    </a>
+                </li>
+                <li className="navigation__item">
+                    <a className="navigation__item-link" href="">
+                        Контакты
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export { Navigation };
