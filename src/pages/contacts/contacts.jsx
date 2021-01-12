45ea64b8aa44c17746f20f6ac501bd229e69690c
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-01-12 14:11:34 +0300

    Исправила имена классов

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -5,7 +5,7 @@ function Contacts() {
         <article className="contacts">
             <h2 className="contacts__header">Контакты компании ООО ТД «УралТехДеталь»</h2>
             <div className="contacts__container">
-                <strong className="contacts__lable">Телефон:</strong>
+                <strong className="contacts__label">Телефон:</strong>
                 <a className="contacts__link" href="tel:+73513264004">
                     +7 (3513) 264-004
                 </a>
@@ -14,7 +14,7 @@ function Contacts() {
                 </a>
             </div>
             <div className="contacts__container">
-                <strong className="contacts__lable">E-mail:</strong>
+                <strong className="contacts__label">E-mail:</strong>
                 <div className="contacts__emails-container">
                     <a className="contacts__link" href="mailto:gruz_74@mail.ru">
                         gruz_74@mail.ru 	
@@ -29,21 +29,21 @@ function Contacts() {
                 </div>
             </div>
             <div className="contacts__container">
-                <strong className="contacts__lable">Фактический адрес:</strong>
+                <strong className="contacts__label">Фактический адрес:</strong>
                 <address className="contacts__address">
                     456300, Челябинская область, город Миасс, Тургоякское шоссе, дом № 3/16,
                     помещение 1
                 </address>
             </div>
             <div className="contacts__container">
-                <strong className="contacts__lable">Почтовый адрес:</strong>
+                <strong className="contacts__label">Почтовый адрес:</strong>
                 <address className="contacts__address">
                     456317, Челябинская область, город Миасс, улица Степана Разина дом № 1,
                     квартира 31
                 </address>
             </div>
             <div>
-                <strong className="contacts__lable">Режим работы:</strong>
+                <strong className="contacts__label">Режим работы:</strong>
                 <ul className="contacts__opening-hours-list">
                     <li className="contacts__opening-hours">
                         местное время: ПН-ПТ 9:00 — 17:00, обед с 12:00 до 12:30 
@@ -53,7 +53,7 @@ function Contacts() {
                     </li>
                 </ul>
             </div>
-            <strong className="contacts__lable-map">Схема проезда</strong>
+            <strong className="contacts__label-map">Схема проезда</strong>
         </article>
     )
 }
