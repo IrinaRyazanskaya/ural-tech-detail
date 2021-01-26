e7ad60e9adf6ac488c03926fd1bc5d2043f7e0be
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-01-26 15:07:39 +0300

    Сверстала модальное окно отправки заявки

---
+++ src/components/request-modal/request-modal.stories.jsx
@@ -0,0 +1,12 @@
+import { RequestModal } from './request-modal';
+
+export default {
+    component: RequestModal,
+    title: 'Components/RequestModal',
+};
+
+export const BasicExample = () => {
+    return (
+        <RequestModal />
+    )
+};
