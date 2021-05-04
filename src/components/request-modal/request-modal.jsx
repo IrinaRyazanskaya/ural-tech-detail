0a55e6d2e82e68027780351b339147564360c78e
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-05-04 21:29:17 +0300

    Интегрировала форму отправки заявки с бекендом

--- src/components/request-modal/request-modal.jsx
+++ src/components/request-modal/request-modal.jsx
@@ -1,102 +1,176 @@
-import './request-modal.css';
-import iconCrossSrc from './cross-icon.svg';
+import { useState } from "react";
+import { applyRequest } from "../../api/apply-request";
+
+import "./request-modal.css";
+import iconCrossSrc from "./cross-icon.svg";
 
 function RequestModal(props) {
-    const handleFileChange = (event) => {
-        const fileName = document.querySelector('.request-modal__file-name');
+  const [buyerName, setBuyerName] = useState("");
+  const [buyerEmail, setBuyerEmail] = useState("");
+  const [buyerPhone, setBuyerPhone] = useState("");
+  const [comment, setComment] = useState("");
+  const [requestFile, setRequestFile] = useState(null);
+  const [requestState, setRequestState] = useState("unknown");
 
-        if (event.target.files.length === 1) {
-            fileName.innerHTML = event.target.files[0].name;
-        } else {
-            fileName.innerHTML = 'Файл не выбран';
-        }
+  const handleFileChange = (event) => {
+    const fileName = document.querySelector(".request-modal__file-name");
+
+    if (event.target.files.length === 1) {
+      fileName.innerHTML = event.target.files[0].name;
+      setRequestFile(event.target.files[0]);
+    } else {
+      fileName.innerHTML = "Файл не выбран";
+      setRequestFile(null);
     }
+  };
 
-    return (
-        <>
-            <div className="request-modal-blackout" />
-            <div className="request-modal">
-                <button 
-                    className="request-modal__close-button"
-                    onClick={props.onClose}
-                >
-                    <img
-                        className="request-modal__close-icon"
-                        src={iconCrossSrc}
-                        alt="Иконка крестика"
-                    />
-                </button>
-                <h2 className="request-modal__header">Отправить заявку</h2>
-                <form className="request-modal__form">
-                    <p className="request-modal__lable">Файл заявки:</p>
-                    <input
-                        className="request-modal__field-file"
-                        type="file"
-                        id="file"
-                        name="file"
-                        multiple={false}
-                        onChange={handleFileChange}
-                    />
-                    <label className="request-modal__file-wrapper" htmlFor="file">
-                        <div className="request-modal__file-name">Файл не выбран</div>
-                        <div className="request-modal__file-button">Выбрать</div>
-                    </label>
-                    <label className="request-modal__lable" htmlFor="name">
-                        Имя: <span className="request-modal__lable-required">*</span>
-                </label><br />
-                    <input
-                        className="request-modal__field"
-                        type="text"
-                        id="name"
-                        name="name"
-                        required
-                    /><br />
-                    <label className="request-modal__lable" htmlFor="email">
-                        E-mail: <span className="request-modal__lable-required">*</span>
-                    </label><br />
-                    <input
-                        className="request-modal__field"
-                        type="email"
-                        id="email"
-                        name="email"
-                        required
-                    /><br />
-                    <label className="request-modal__lable" htmlFor="phone">
-                        Телефон: <span className="request-modal__lable-required">*</span>
-                    </label><br />
-                    <input
-                        className="request-modal__field"
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        required
-                    />
-                    <p className="request-modal__lable">Комментарий к заявке:</p>
-                    <textarea
-                        className="request-modal__comment"
-                        name="comment"
-                        id="comment"
-                        rows="5"
-                    >
-                    </textarea>
-                    <p className="request-modal__form-text">
-                        Отправляя данную форму, я подтверждаю, что ознакомлен с&nbsp;
-                    <a className="request-modal__form-link" target="_blank" href="/privacy">
-                            политикой конфиденциальности
-                    </a>
-                    &nbsp;и согласен на хранение и обработку персональных данных.
-                </p>
-                    <div className="request-modal__button-container">
-                        <input
-                            className="request-modal__send-button"
-                            type="submit"
-                            value="ОТПРАВИТЬ"
-                        />
-                    </div>
-                </form>
-            </div>
-        </>
-    )
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setRequestState("pending");
+    applyRequest(buyerName, buyerEmail, buyerPhone, comment, requestFile)
+      .then(() => {
+        setRequestState("success");
+      })
+      .catch(() => {
+        setRequestState("failure");
+      });
+  };
+
+  const form = (
+    <form className="request-modal__form" onSubmit={handleFormSubmit}>
+      <p className="request-modal__lable">Файл заявки:</p>
+      <input
+        className="request-modal__field-file"
+        type="file"
+        id="file"
+        name="file"
+        multiple={false}
+        onChange={handleFileChange}
+      />
+      <label className="request-modal__file-wrapper" htmlFor="file">
+        <div className="request-modal__file-name">Файл не выбран</div>
+        <div className="request-modal__file-button">Выбрать</div>
+      </label>
+      <label className="request-modal__lable" htmlFor="name">
+        Имя: <span className="request-modal__lable-required">*</span>
+      </label>
+      <br />
+      <input
+        className="request-modal__field"
+        type="text"
+        id="name"
+        name="name"
+        value={buyerName}
+        onChange={(event) => {
+          setBuyerName(event.target.value);
+        }}
+        required
+      />
+      <br />
+      <label className="request-modal__lable" htmlFor="email">
+        E-mail: <span className="request-modal__lable-required">*</span>
+      </label>
+      <br />
+      <input
+        className="request-modal__field"
+        type="email"
+        id="email"
+        name="email"
+        value={buyerEmail}
+        onChange={(event) => {
+          setBuyerEmail(event.target.value);
+        }}
+        required
+      />
+      <br />
+      <label className="request-modal__lable" htmlFor="phone">
+        Телефон: <span className="request-modal__lable-required">*</span>
+      </label>
+      <br />
+      <input
+        className="request-modal__field"
+        type="tel"
+        id="phone"
+        name="phone"
+        value={buyerPhone}
+        onChange={(event) => {
+          setBuyerPhone(event.target.value);
+        }}
+        required
+      />
+      <p className="request-modal__lable">Комментарий к заявке:</p>
+      <textarea
+        className="request-modal__comment"
+        name="comment"
+        id="comment"
+        rows="5"
+        value={comment}
+        onChange={(event) => {
+          setComment(event.target.value);
+        }}
+      ></textarea>
+      <p className="request-modal__form-text">
+        Отправляя данную форму, я подтверждаю, что ознакомлен с&nbsp;
+        <a className="request-modal__form-link" target="_blank" href="/privacy">
+          политикой конфиденциальности
+        </a>
+        &nbsp;и согласен на хранение и обработку персональных данных.
+      </p>
+      <div className="request-modal__button-container">
+        <input
+          className="request-modal__send-button"
+          type="submit"
+          value="ОТПРАВИТЬ"
+        />
+      </div>
+    </form>
+  );
+
+  const pending = <div>Заявка отправляется</div>;
+
+  const success = <div>Форма отправлена</div>;
+
+  const failure = (
+    <div>
+      Что-то пошло не так
+      <button
+        onClick={() => {
+          setRequestState("unknown");
+        }}
+      >
+        Попробовать еще раз
+      </button>
+    </div>
+  );
+
+  let content;
+
+  if (requestState === "pending") {
+    content = pending;
+  } else if (requestState === "success") {
+    content = success;
+  } else if (requestState === "failure") {
+    content = failure;
+  } else {
+    content = form;
+  }
+
+  return (
+    <>
+      <div className="request-modal-blackout" />
+      <div className="request-modal">
+        <button className="request-modal__close-button" onClick={props.onClose}>
+          <img
+            className="request-modal__close-icon"
+            src={iconCrossSrc}
+            alt="Иконка крестика"
+          />
+        </button>
+        <h2 className="request-modal__header">Отправить заявку</h2>
+        {content}
+      </div>
+    </>
+  );
 }
 
 export { RequestModal };
